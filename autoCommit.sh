7d9709a7fa0cd56edc9f9c2a61270d994497b815
bhwa233 <404174262@qq.com>
2022-06-18 15:30:25 +0800

    auto commit

--- autoCommit.sh
+++ autoCommit.sh
@@ -3,3 +3,5 @@ git status
 git add .
 git commit -m "auto commit"
 git push origin master
+
+git remote set-url origin https://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@github.com/lxw15337674/blog.git
---
+++ techDoc.md
@@ -0,0 +1,6 @@
+# techDoc
+记录那些有价值的技术文档:
+
+
+[JavaScript 内存泄漏教程](http://www.ruanyifeng.com/blog/2017/04/memory-leak.html)
+
